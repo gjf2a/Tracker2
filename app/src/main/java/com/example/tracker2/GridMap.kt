0e671c1f5ff87321b94bcc9f09998f244785a701
gjf2a <ferrer@hendrix.edu>
2021-04-04 19:40:50 -0500

    Debugging resizing of GridMap.

--- app/src/main/java/com/example/tracker2/GridMap.kt
+++ app/src/main/java/com/example/tracker2/GridMap.kt
@@ -1,34 +1,83 @@
 package com.example.tracker2
 
+import java.lang.StringBuilder
 import java.util.*
 
-class GridMap(val cellsPerMeter: Double, val metersPerSide: Double) {
-    val cellsPerSide = (cellsPerMeter * metersPerSide).toInt()
-    private val cells = BitSet(cellsPerSide * cellsPerSide)
+fun cell2index(xCell: Int, yCell: Int, cellsPerSide: Int): Int {
+    val x = xCell + cellsPerSide/2
+    val y = yCell + cellsPerSide/2
+    return y * cellsPerSide + x
+}
+
+class GridMap(val cellsPerMeter: Double, var metersPerSide: Double = 2.5) {
+    private var cells = makeCells()
+    fun cellsPerSide() = (cellsPerMeter * metersPerSide).toInt()
+    fun totalCells() = cellsPerSide() * cellsPerSide()
+
+    private fun makeCells() = BitSet(totalCells())
 
     private fun meter2cell(meter: Double): Int = (meter * cellsPerMeter).toInt()
-
     private fun meters2index(xMeter: Double, yMeter: Double): Int =
-        meter2cell(yMeter + metersPerSide/2) * cellsPerSide + meter2cell(xMeter + metersPerSide/2)
+        meter2cell(yMeter + metersPerSide/2) * cellsPerSide() + meter2cell(xMeter + metersPerSide/2)
+
+    fun setAll() {
+        cells.set(0, totalCells(), true)
+    }
 
     fun set(xMeter: Double, yMeter: Double, width: Double, height: Double, value: Boolean) {
         var m = yMeter
         while (m < yMeter + height) {
-            cells.set(meters2index(xMeter, m), meters2index(xMeter + width, m), value)
+            val start = meters2index(xMeter, m)
+            val end = meters2index(xMeter + width, m)
+            if (start < 0 || start >= cellsPerSide() || end < 0 || end >= cellsPerSide()) {
+                resize()
+            }
+            cells.set(start, end, value)
             m += 1.0/cellsPerMeter
+        }
+    }
+
+    private fun resize() {
+        val oldCells = cells
+        val oldSize = cellsPerSide()
+        println("oldSize: $oldSize")
+        val oldStart = -oldSize/2
+        metersPerSide *= 2
+        println("newSize: ${cellsPerSide()}")
+        cells = makeCells()
+        for (x in oldStart until oldStart + oldSize) {
+            for (y in oldStart until oldStart + oldSize) {
+                cells.set(cell2index(x, y, cellsPerSide()), oldCells.get(cell2index(x, y, oldSize)))
+            }
         }
     }
 
     fun isFilled(xMeter: Double, yMeter: Double): Boolean = cells.get(meters2index(xMeter, yMeter))
 
-    fun print() {
-        for (y in 0 until cellsPerSide) {
-            for (x in 0 until cellsPerSide) {
-                print("${if (cells.get(y * cellsPerSide + x)) {"*"} else {"."}}")
+    override fun toString(): String {
+        val builder = StringBuilder()
+        for (y in 0 until cellsPerSide()) {
+            for (x in 0 until cellsPerSide()) {
+                builder.append(if (cells.get(y * cellsPerSide() + x)) {"*"} else {"."})
             }
-            println()
+            builder.append('\n')
+        }
+        return builder.toString()
+    }
+}
+
+fun mapFrom(groundline: ArrayList<Int>, cellsPerMeter: Double, converter: PixelConverter): GridMap {
+    val map = GridMap(cellsPerMeter)
+    for (x in groundline.indices) {
+        val x1 = converter.xPixel2distance(x, groundline[x])
+        val x2 = converter.xPixel2distance(x + 1, groundline[x])
+        val y1 = converter.yPixel2distance(groundline[x])
+        val y2 = converter.yPixel2distance(groundline[x] - 1)
+        if (y1 < MAX_DISTANCE_METERS) {
+            map.set(x1, y1, x2 - x1, y2 - y1, true)
         }
     }
+    return map
 }
 
 const val MAX_DISTANCE_METERS = 2.0
